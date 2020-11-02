b2a779f8072bf520e2c1e232e7a6ac793765854a
Andrii Kozakevych <andriikozakevych@gmail.com>
2020-11-02 23:48:04 +0200

    create components in different ways

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import CreateElementExample from './components/CreateElementExample';
+import ComponentExample from './components/ComponentExample';
+import PureComponentExample from './components/PureComponentExample';
+
+function App() {
+  return (
+    <div>
+      <CreateElementExample />
+      <ComponentExample />
+      <PureComponentExample />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/CreateElementExample.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CreateElementExample() {
+  return (
+    React.createElement('h1', {}, 'Hello, world !')
+  );
+}
+
+export default CreateElementExample;

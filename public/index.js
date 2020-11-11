06aefe7edcf12ca7638b961d43b55d6198a7423f
Andrii Kozakevych <andriikozakevych@gmail.com>
2020-11-11 07:33:35 +0200

    add jest tests

--- public/index.js
+++ public/index.js
@@ -1,4 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from '../src/App.js';
 
-ReactDOM.render(<div>Welcome to the app!</div>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const App = () => {
+    return (
+        <div>
+            Welcome to the app!
+        </div>
+    )
+};
+export default App;

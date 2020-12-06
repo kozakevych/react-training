2b9373c9fb11ac1afd25fdc1d771a0cb4c5f0414
Andrii Kozakevych <andriikozakevych@gmail.com>
2020-12-06 22:53:21 +0200

    add styling for modal window

--- src/components/ModalWindowBody.js
+++ src/components/ModalWindowBody.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Button from '@material-ui/core/Button';
 import InputLabel from '@material-ui/core/InputLabel';
 import FormControl from '@material-ui/core/FormControl';
@@ -7,39 +7,55 @@ import Typography from '@material-ui/core/Typography';
 
 import './ModalWindowBody.scss';
 
-const ModalWindowBody = (data) => {
+const ModalWindowBody = (data, submitCallback, resetCallback) => {
     const {title, createdAt, movieUrl, genre, overview, runtime} = data
+
+    const handleSubmitClick = () => {
+        submitCallback();
+    }
+
+    const handleResetClick = () => {
+        resetCallback();
+    }
+
+    useEffect(() => {
+        // TODO: Make form editable
+        console.log(data)
+    }, [data])
+
     return (
         <div className="modal-window-wrapper">
-            <Typography variant="h4" gutterBottom>
-            Edit Movie
+            <Typography variant="h5" gutterBottom className="modal-window-title">
+                Edit Movie
             </Typography>
-            <FormControl fullWidth>
-                <InputLabel htmlFor="standard-adornment-amount" style={{color: 'black'}}>Title</InputLabel>
+            <FormControl fullWidth className="form-control-wrapper">
+                <InputLabel htmlFor="standard-adornment-amount">Title</InputLabel>
                 <Input color="secondary" id="standard-adornment-amount" value={title || ''} />
             </FormControl>
-            <FormControl fullWidth>
-                <InputLabel htmlFor="standard-adornment-amount" style={{color: 'black'}}>Release Date</InputLabel>
+            <FormControl fullWidth className="form-control-wrapper">
+                <InputLabel htmlFor="standard-adornment-amount">Release Date</InputLabel>
                 <Input color="secondary" id="standard-adornment-amount" value={createdAt || ''} />
             </FormControl>
-            <FormControl fullWidth>
-                <InputLabel htmlFor="standard-adornment-amount" style={{color: 'black'}}>Movie Url</InputLabel>
+            <FormControl fullWidth className="form-control-wrapper">
+                <InputLabel htmlFor="standard-adornment-amount">Movie Url</InputLabel>
                 <Input color="secondary" id="standard-adornment-amount" value={movieUrl || ''} />
             </FormControl>
-            <FormControl fullWidth>
-                <InputLabel htmlFor="standard-adornment-amount" style={{color: 'black'}}>Genre</InputLabel>
+            <FormControl fullWidth className="form-control-wrapper">
+                <InputLabel htmlFor="standard-adornment-amount">Genre</InputLabel>
                 <Input color="secondary" id="standard-adornment-amount" value={genre || ''} />
             </FormControl>
-            <FormControl fullWidth>
-                <InputLabel htmlFor="standard-adornment-amount" style={{color: 'black'}}>Overview</InputLabel>
+            <FormControl fullWidth className="form-control-wrapper">
+                <InputLabel htmlFor="standard-adornment-amount">Overview</InputLabel>
                 <Input color="secondary" id="standard-adornment-amount" value={overview || ''} />
             </FormControl>
-            <FormControl fullWidth>
-                <InputLabel htmlFor="standard-adornment-amount" style={{color: 'black'}}>Runtime</InputLabel>
+            <FormControl fullWidth className="form-control-wrapper">
+                <InputLabel htmlFor="standard-adornment-amount">Runtime</InputLabel>
                 <Input color="secondary" id="standard-adornment-amount" value={runtime || ''} />
             </FormControl>
-                <Button variant="contained">Reset</Button>
-                <Button variant="contained" color="secondary">Submit</Button>
+            <div className="button-wrapper">
+                <Button className="reset-button" variant="contained" onClick={handleResetClick}>Reset</Button>
+                <Button className="submit-button" variant="contained" onClick={handleSubmitClick} color="secondary">Submit</Button>
+            </div>
         </div>
     )
 }
